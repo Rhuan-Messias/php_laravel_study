41b4a43334416cedbe3239af76a4c64c8be0d512
Rhuan-Messias <messias.ifg@gmail.com>
2023-12-25 09:26:50 -0300

    small change

--- web_dev_avançado/app_super_gestao/app/Fornecedor.php
+++ web_dev_avançado/app_super_gestao/app/Fornecedor.php
@@ -8,6 +8,7 @@ class Fornecedor extends Model
 {
     //
     protected $table = 'fornecedores'; //protected are private attributes
+    
     //abaixo nós autorizamos com o método protected $fillable que o método
     //create através de um array associativo passe os parâmetros como métodos
     //do próprio objeto model, para que possam ser persistidos 
