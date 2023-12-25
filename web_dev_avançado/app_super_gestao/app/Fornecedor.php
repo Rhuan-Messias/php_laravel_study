3b26dcd56c88c2a9be3eed1c2ecbac5f0035d6de
Rhuan-Messias <messias.ifg@gmail.com>
2023-12-25 10:02:35 -0300

    softdeletes add

--- web_dev_avançado/app_super_gestao/app/Fornecedor.php
+++ web_dev_avançado/app_super_gestao/app/Fornecedor.php
@@ -3,14 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
-    //
+    //trait softdeletes
+    use SoftDeletes;
     protected $table = 'fornecedores'; //protected are private attributes
-    
-    //abaixo nós autorizamos com o método protected $fillable que o método
-    //create através de um array associativo passe os parâmetros como métodos
-    //do próprio objeto model, para que possam ser persistidos 
+    //abaixo nós autorizamos com o método protected $fillable que o método create através de um array associativo passe os parâmetros como métodos do próprio objeto model, para que possam ser persistidos 
     protected $fillable = ['nome','site','uf','email']; 
+
+
 }
