d17d0ebf1bde6a915292a82c49fa9c420c577ab3
Rhuan-Messias <messias.ifg@gmail.com>
2023-12-26 17:48:57 -0300

    validação e old input dos dados

--- web_dev_avançado/app_super_gestao/app/Http/Controllers/ContatoControler.php
+++ web_dev_avançado/app_super_gestao/app/Http/Controllers/ContatoControler.php
@@ -8,31 +8,26 @@ use App\SiteContato;
 class ContatoControler extends Controller
 {
     public function contato(Request $request) {
-        /*
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br/>';
-        echo $request->input('email');
-        */
+     
+        $motivo_contatos=[
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view("site.contato",["titulo"=>'Contato','motivo_contatos'=>$motivo_contatos]);
+    }
 
-        /*
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->email = $request->input('email');
-        $contato->telefone = $request->input('telefone');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-        //print_r($contato->getAttributes());
-        $contato->save();
-        */
+    public function salvar(Request $request) {
+        
+        //realizar a validação dos dados do formulário recebidos em request
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
 
-        $contato = new SiteContato();
-        $contato->create($request->all());
-        //$contato->save();
-        //print_r($contato->getAttributes());
-
-        return view("site.contato",["titulo"=>'Contato']);
+        //SiteContato::create($request->all());
     }
 }
